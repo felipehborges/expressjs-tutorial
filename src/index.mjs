7cc119c2e6b0342ee3f2e5147d337c20b492f2c9
Hamasaki <felipehama@gmail.com>
2025-05-26 17:49:48 -0300

    passportjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,20 +1,58 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import session from "express-session";
+import passport from "passport";
 import routes from "./routes/index.mjs";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser("helloworld"));
+app.use(
+  session({
+    secret: "hamasaki the dev",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (request, response) => {
-  response.cookie("hello", "world", { maxAge: 60000 * 100 });
-  response.status(201).send({ msg: "Main route is here!" });
-});
-
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
+});
+
+app.get("/", (request, response) => {
+  console.log(request.session);
+  console.log(request.session.id);
+  request.session.visited = true;
+  // this option tracks the user,
+  // it doesn't generate a random ID in every request anymore
+
+  response.cookie("hello", "world", { maxAge: 30000, signed: true });
+  response.status(201).send({ msg: "Hello" });
+});
+
+app.post("/api/auth", (request, response) => {
+  const {
+    body: { username, password },
+  } = request;
+
+  const findUser = mockUsers.find((user) => user.username === username);
+
+  if (!findUser || findUser.password !== password)
+    return response.status(401).send({ msg: "BAD CREDENTIALS" });
+
+  request.session.user = findUser;
+
+  return response.status(200).send(findUser);
 });
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,7 +1,12 @@
 export const mockUsers = [
-  { id: 1, username: "john", displayName: "John" },
-  { id: 2, username: "mary", displayName: "Mary" },
-  { id: 3, username: "carl", displayName: "Carl" },
-  { id: 4, username: "jane", displayName: "Jane" },
-  { id: 5, username: "doe", displayName: "Doe" },
+  { id: 1, username: "john", displayName: "John", password: "john123" },
+  { id: 2, username: "jane", displayName: "Jane", password: "jane123" },
+  { id: 3, username: "doe", displayName: "Doe", password: "doe123" },
+  { id: 4, username: "alice", displayName: "Alice", password: "alice123" },
+  { id: 5, username: "bob", displayName: "Bob", password: "bob123" },
+  { id: 6, username: "carl", displayName: "Carl", password: "carl123" },
+  { id: 7, username: "dave", displayName: "Dave", password: "dave123" },
+  { id: 8, username: "eve", displayName: "Eve", password: "eve123" },
+  { id: 9, username: "frank", displayName: "Frank", password: "frank123" },
+  { id: 10, username: "grace", displayName: "Grace", password: "grace123" },
 ];
